5461eb0d5df96ab708d08b5f0370e48b04214dd9
0xWANN <132135447+wannqn@users.noreply.github.com>
2025-08-14 16:36:56 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/sound_trigger_platform_info.xml
 
 # Camera (MIUI)
-$(call inherit-product-if-exists, device/xiaomi/camera-renoir/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Display
 PRODUCT_COPY_FILES += \
