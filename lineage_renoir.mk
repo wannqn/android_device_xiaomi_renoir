d5018c6581162e8880564d4bb4684057f094347a
0xWANN <132135447+wannqn@users.noreply.github.com>
2025-08-15 19:44:40 +0300

    Update lineage_renoir.mk

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -10,6 +10,25 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Rising flags
+RISING_MAINTAINER := wannq
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Qualcomm Snapgragon 780G" \
+    RisingMaintainer="wannq"
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := true
+
+# Whether to ship lawnchair launcher, false by default
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false 
+
+# GMS
+WITH_GMS := true
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
