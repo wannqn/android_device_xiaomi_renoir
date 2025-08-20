222cb503cf1e3f9480eaf4e349e15b8090d1640b
0xWANN <132135447+wannqn@users.noreply.github.com>
2025-08-20 13:51:22 +0300

    Update and rename lineage_renoir.mk to cherish_renoir.mk

--- cherish_renoir.mk
+++ cherish_renoir.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9G
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := cherish_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
